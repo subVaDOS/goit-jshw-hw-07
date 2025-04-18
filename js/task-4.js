3fb552d5acb80527805b2ebdb682e0a4f69064bd
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-18 18:25:57 +0300

    modified file ./js/task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    return alert('All form fields must be filled in');
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  form.reset();
+});
